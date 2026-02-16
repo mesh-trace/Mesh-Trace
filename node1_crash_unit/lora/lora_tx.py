4eec46a79c87a617d89dd8c1f82ee5b00dc8c777
Avdhut Gogawale <avdhutgogawale@gmail.com>
2026-02-16 23:22:53 +0530

    encryption added for packet

--- node1_crash_unit/lora/lora_tx.py
+++ node1_crash_unit/lora/lora_tx.py
@@ -44,7 +44,6 @@ class LoRaCrashTX(LoRa):
         self.set_spreading_factor(7)   # SF7
         self.set_coding_rate(1)        # 4/5
         self.set_sync_word(0x34)
-        self.set_crc(True)
         self.set_pa_config(pa_select=1, max_power=0x70, output_power=0x0F)
 
         print("[INFO] LoRa Crash TX initialized")
