5e1c8bd8495c625ebd668835d75e243f6cd4b263
Avdhut Gogawale <avdhutgogawale@gmail.com>
2026-06-15 20:09:51 +0530

    threshold adjusted according to rc car

--- node1_crash_unit/main.py
+++ node1_crash_unit/main.py
@@ -146,9 +146,9 @@ class CrashDetectionUnit:
         if not impact_confirmed:
             return False, None, None
 
-        if accel_mag < 15:
+        if accel_mag < 5:
             severity = "LOW"
-        elif accel_mag < 25:
+        elif accel_mag < 8:
             severity = "MEDIUM"
         else:
             severity = "HIGH"
